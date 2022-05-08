5392caa4cdd00bf5754ef0a86b567531b7f18c65
okello daniel <handsdani@gmail.com>
2022-05-08 14:52:27 +0300

    project well

--- api.py
+++ api.py
@@ -1,3 +1,22 @@
 from flask import Flask, request
 import requests
+from blockchain import Blockchain
+import json
 
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/chain', methods=['GET'])
+
+def get_chain():
+    chain_data = []
+
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    
+    return json.dumps({
+        'length': len(chain_data),
+        'chain': chain_data
+    })
+app.run(debug=True, port=5000)

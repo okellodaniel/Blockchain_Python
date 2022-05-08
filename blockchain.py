807b99e9fac8a0446364091e17317a71a78b1f1c
okello daniel <handsdani@gmail.com>
2022-05-08 14:33:48 +0300

    defined the consensus lofic

--- blockchain.py
+++ blockchain.py
@@ -18,3 +18,14 @@ class Blockchain:
     @property
     def last_block(self):
         return self.chain[-1]
+    
+    difficulty = 2
+
+    def proof_of_work(self):
+        block.nonce = computed_hash = block.compute_hash()
+
+        while not computed_hash.startswith('0'*Blockchain.difficulty):
+        block.nonce += 1
+        computed_hash = block.compute_hash
+        
+        return computed_hash
